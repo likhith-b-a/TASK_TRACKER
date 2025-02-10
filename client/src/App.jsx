703605f894c25c0d3f5c1ee4aa7091a75613020d
likhith-b-a <likhithba51@gmail.com>
2025-02-10 16:47:05 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
       <div className="tasks">
         <TaskCard id={"67a9a53aa504fa6ab6c89515"} />
         <TaskCard id={"67a9a53aa504fa6ab6c89514"} />
+        <TaskCard id={"67a9df5fd90ab600c9e0d15b"} />
       </div>
     </div>
   );
