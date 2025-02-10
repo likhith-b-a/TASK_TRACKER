b43bccaeb093e79eb8e7980bf0033abdc6b85337
likhith-b-a <likhithba51@gmail.com>
2025-02-10 14:49:54 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // Your backend URL
+        target: "https://task-tracker-cn3a.onrender.com", // Your backend URL
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
