957d50c91c4a9337140e5002e413b05e3e315b83
likhith-b-a <likhithba51@gmail.com>
2025-02-13 21:50:47 +0530

    Adding getStreak route

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   completionDays,
   addCompletionDay,
   fetchDetails,
+  getStreak,
 } from "../controllers/userControllers.js"
 
 const router = Router();
@@ -11,6 +12,7 @@ const router = Router();
 router.route("/:id/completionDays").get(completionDays);
 router.route("/:id/completionDays/add").post(addCompletionDay);
 router.route("/:id/details").get(fetchDetails)
+router.route("/:id/streak").get(getStreak)
 
 
 export default router;
